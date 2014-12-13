82f6a5e34a869c8f4477d94f34aed8199e8c1ca4
jkupcho <dardo@dardos-Mac-Pro.local>
2014-12-12 22:23:27 -0600

    started adding web service calls

---
+++ dota2-fantasy/src/test/java/com/mke/meetup/dota2/fantasy/service/PlayerServiceTest.java
@@ -0,0 +1,11 @@
+package com.mke.meetup.dota2.fantasy.service;
+
+import org.junit.Test;
+
+public class PlayerServiceTest {
+	
+	@Test
+	public void env() {
+		System.out.println(System.getenv("STEAM_API_KEY"));
+	}
+}
